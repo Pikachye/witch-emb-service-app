d42cfdc0f036210e3eba33dfaabe905fd9f8fa53
Pikachye <bigcat13@rambler.ru>
2025-02-11 02:10:17 +0300

    Reload images

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-        const images = ["20241222_154239.jpg"];
+        const images = ["20241222_154239.jpg","20250114_230828.jpg","20250130_212223.jpg","20250205_215602 (1).jpg"];
         function loadImages() {
             const imageFolder = "images/";
             const gallery = document.getElementById("gallery");
             gallery.innerHTML = ""; // Очищаем галерею
             images.forEach(image => {
                 const img = document.createElement("img");
-                img.src = imageFolder + image + "?v=1739228814687";
+                img.src = imageFolder + image + "?v=1739229017261";
                 img.alt = image;
                 gallery.appendChild(img);
             });
